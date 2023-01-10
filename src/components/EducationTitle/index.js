3ff150c269136fca13412e32c20989bb43288a4e
Andreza de Sousa <62725350+andrezadesousa@users.noreply.github.com>
2023-01-10 14:47:38 -0300

    fix: create a new component

---
+++ src/components/EducationTitle/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+export function EducationTitle(props) {
+  return (
+    <h3 className="education__title">{props.title}</h3>
+  )
+}
--- src/components/LearnData/index.js
+++ src/components/LearnData/index.js
@@ -1,17 +1,18 @@
 import "./styles.css";
 
 import { EducationContent } from "../EducationContent/index"
-import { BgGrid } from "../BgGrid/index";
 import { MarkingTime } from "../../components/MarkingTime/index";
+import { BgGrid } from "../BgGrid/index";
+import { EducationTitle } from "../EducationTitle";
 // import { MarkingTimeEnd } from "../../components/MakingTimeEnd/index";
 
 export function LearnData() {
   return (
     <>
     <EducationContent>
-      <MarkingTime></MarkingTime>
+      <MarkingTime />
       <BgGrid id="learn__data">
-        <h3 className="learn__title">FrontStart</h3>
+        <EducationTitle title="FrontStart" />
         <span className="learn__studies">Isadora Stangarlin</span>
         <span className="learn__year">Jan/2023 - estudando</span>
       </BgGrid>
